eb668d4d2314f1c33c23d84f47aa612010e28858
aci456852 <1084665808@qq.com>
2019-02-07 00:00:58 +0800

    localhost not ok

---
+++ learnVue/src/components/learn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="vue">Hello Vue.js! {{ message }}</div>
+</template>
+
+<script type="text/javascript">
+  export default { 
+    name: "learn",
+    data (){ 
+      return {
+        message: "lzy冲鸭"
+      }
+    }
+  }
+</script>
+
+<style type="text/css">
+  #vue{
+    color: green;
+    font-size: 28px;
+  }
+</style>
