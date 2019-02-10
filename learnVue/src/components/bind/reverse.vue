94b60c27e67480d3fab9f571689293b866324eb1
aci456852 <1084665808@qq.com>
2019-02-10 22:53:24 +0800

    练习：字符串反转

---
+++ learnVue/src/components/bind/reverse.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+	    <p>{{ message }}</p>
+	    <button v-on:click="reverseMessage">反转字符串</button>
+	</div>
+</template>
+    
+<script>
+export default {
+	el: '#app',
+    data() {
+      return {
+        message: '你倒是显示呀!!!'
+      }
+    },
+  	methods: {
+      reverseMessage: function () {
+      	this.message = this.message.split('').reverse().join('')
+      }
+  	}
+}
+</script>
